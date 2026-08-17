11b5fbec28adb90ecb7b0ec138e2c6f936915ea5
Lilia Mkoyan <mkoyanlilia@gmail.com>
2026-08-17 12:32:27 +0400

    Add initial user migration and database setup

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Task4.UserManagement.Models;
+
+namespace Task4.UserManagement.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<User>  Users { get; set; }
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+}
---
+++ Models/User.cs
@@ -0,0 +1,18 @@
+﻿namespace Task4.UserManagement.Models;
+public enum AccountStatus
+{
+    Unverified,
+    Active,
+    Blocked
+}
+
+public class User
+{
+    public int Id  { get; set; }
+    public string Name { get; set; }
+    public string Email  { get; set; }
+    public string PasswordHash  { get; set; }
+    public DateTime LastActive  { get; set; }
+    public DateTime RegisteredTime  { get; set; }
+    public AccountStatus Status { get; set; }
+}
