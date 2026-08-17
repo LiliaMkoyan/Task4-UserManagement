a49adbd6c0c6da9b48752f8988e7398b36a67292
Lilia Mkoyan <mkoyanlilia@gmail.com>
2026-08-17 12:32:27 +0400

    Add initial user migration and database setup

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Task4.UserManagement.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
