cd97c1cee7e7f73ad35b76f6eb55033003f38027
Lilia Mkoyan <mkoyanlilia@gmail.com>
2026-08-20 00:33:55 +0400

    Accomplished local version of the project before deployment

---
+++ Controllers/UserController.cs
@@ -0,0 +1,6 @@
+﻿namespace Task4.UserManagement.Controllers;
+
+public class UserController
+{
+    
+}
---
+++ Middleware/UserStatusMiddleware.cs
@@ -0,0 +1,6 @@
+﻿namespace Task4.UserManagement.Middleware;
+
+public class UserStatusMiddleware
+{
+    
+}
